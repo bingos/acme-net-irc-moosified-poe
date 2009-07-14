a467de0752c40db04fcbf232bd9906602bd72b2a
Chris Williams <chris@bingosnet.co.uk>
2009-07-14 10:49:31 +0100

    Some updates

--- lib/Acme/Net/IRC/Moosified/POE.pm
+++ lib/Acme/Net/IRC/Moosified/POE.pm
@@ -2,11 +2,46 @@ package Acme::Net::IRC::Moosified::POE;
 
 use strict;
 use warnings;
+use POE;
 use vars qw($VERSION);
 
 $VERSION = '0.02';
 
+use MooseX::POE;
+use Acme::Net::IRC::Moosified::POE::Connection;
 
+has 'debug' => (
+  is => 'rw',
+  default => sub {0},
+);
+
+sub START {
+  my ($kernel,$session) = @_[KERNEL,SESSION];
+  $kernel->refcount_increment( $session->ID(), __PACKAGE__ );
+  return;
+}
+
+sub newconn {
+  my $self = shift;
+  $self->call( '_newconn', @_ );
+}
+
+event '_newconn' => sub {
+  my ($kernel,$self,@args) = @_[KERNEL,OBJECT,ARG0..$#_];
+  return Acme::Net::IRC::Moosified::POE::Connection->new( $self, @args );
+};
+
+sub do_one_loop {
+  $poe_kernel->run_one_timeslice();
+}
+
+sub start {
+  $poe_kernel->run();
+}
+
+no MooseX::POE;
+
+__PACKAGE__->meta->make_immutable;
 
 'Moosey Moosey';
 
--- lib/Acme/Net/IRC/Moosified/POE/Connection.pm
+++ lib/Acme/Net/IRC/Moosified/POE/Connection.pm
@@ -2,10 +2,151 @@ package Acme::Net::IRC::Moosified::POE::Connection;
 
 use strict;
 use warnings;
+use POE::Component::IRC;
+use POE::Component::IRC::Common qw(:ALL);
 use vars qw($VERSION);
 
 $VERSION = '0.02';
 
+use MooseX::POE;
+use Moose::Util::TypeConstraints;
+
+has 'nickname' => (
+  is => 'ro',
+  default => sub { $ENV{IRCNICK} || eval { scalar getpwuid($>) } || $ENV{USER} || $ENV{LOGNAME} || 'WankerBot' },
+  isa => subtype 'Str' => where { /^[A-Za-z_0-9`\-^\|\\\{}\[\]]+$/ },
+);
+
+has 'username' => (
+  is => 'ro',
+  default => sub { eval { scalar getpwuid($>) } || $ENV{USER} || $ENV{LOGNAME} || 'japh' },
+);
+
+has 'ircname' => (
+  is => 'ro',
+  default => sub { $ENV{IRCNAME} || eval { (getpwuid($>))[6] } || 'Just Another Perl Hacker' },
+);
+
+has 'parent' => (
+  is => 'ro',
+  isa => 'Acme::Net::IRC::Moosified::POE',
+);
+
+has 'port' => (
+  is => 'ro',
+  default => sub { 6667 },
+  isa => subtype 'Int' => where { $_ > 0 and $_ <= 65535 },
+);
+
+has 'hostname' => (
+  is => 'ro',
+);
+
+has 'server' => (
+  is => 'ro',
+);
+
+sub BUILDARGS {
+  my $class = shift;
+  my $parent = shift;
+  my %args = @_;
+  $args{lc $_} = delete $args{$_} for keys %args;
+  $args{parent} = $parent;
+  return $class->SUPER::BUILDARGS(%args);
+}
+
+# Prints a message to the defined error filehandle(s).
+# No further description should be necessary.
+
+sub printerr {
+  shift;
+  print STDERR @_, "\n";
+}
+
+# Prints a message to the defined output filehandle(s).
+sub print {
+  shift;
+  print STDOUT @_, "\n";
+}
+
+no MooseX::POE;
+
 'This is where the magic happens';
 
 __END__
+
+IRC commands:
+
+admin
+away
+ctcp
+ctcp_reply
+info
+invite
+ison
+join
+kick
+links
+list
+lusers
+me
+mode
+motd
+names
+new_chat
+new_get
+new_send
+nick
+notice
+oper
+part
+privmsg
+quit
+rehash
+restart
+sconnect
+squit
+stats
+summon
+timestamp
+topic
+trace
+userhost
+users
+version
+wallops
+who
+whois
+whowas
+
+new
+AUTOLOAD
+$meth
+_add_generic_handler
+add_global_handler
+add_handler
+add_default_handler
+connect
+connected
+debug
+dequote
+DESTROY
+disconnect
+error
+format
+handler
+ignore
+maxlinelen
+parse
+parse_ctcp
+parse_num
+peer
+printerr
+print
+schedule
+schedule_output_event
+server
+sl
+sl_real
+unignore
+_default
